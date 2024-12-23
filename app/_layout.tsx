ed5b49934b0ef6d69fb24d4e6d7fe11137bdaecb
neuenm <neuenmobilia@gmail.com>
2024-12-22 23:51:04 -0300

    Refactor AuthProvider export to default and update import in layout

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Stack } from 'expo-router';
-import { AuthProvider } from './AuthContext';
+import AuthProvider from './AuthContext';
 import './global.css';
 
 export default function Layout() {

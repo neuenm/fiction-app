a89a3c36af2c00d4fa4de1c238ff17e155bb912a
neuenm <neuenmobilia@gmail.com>
2024-12-22 23:52:29 -0300

    Add book detail view and enhance book list with loading state and header

--- app/(auth)/books/index.jsx
+++ app/(auth)/books/index.jsx
@@ -1,72 +1,99 @@
-import { Text, TouchableOpacity, ScrollView } from 'react-native';
+import React, { useEffect, useState, useContext } from 'react';
+import { Text, ScrollView, RefreshControl, View, TouchableWithoutFeedback } from 'react-native';
 import NetInfo from '@react-native-community/netinfo';
-import AsyncStorage from '@react-native-async-storage/async-storage';
-import React, { useEffect, useState } from 'react';
-import { AuthContext } from '../../AuthContext';
-import { useContext } from 'react';
 import BookCard from './components/bookCard';
-import { getBooksFromStorage } from '~/lib/utils';
+import { getBooksFromStorage, saveBooksToStorage } from '~/lib/utils';
 import fetchServer from '~/lib/fetchServer';
+import { Skeleton } from '~/components/ui/skeleton';
+import Header from '~/components/ui/header';
+import { AuthContext } from '~/app/AuthContext';
 
 export default function Index() {
+  const { userToken } = useContext(AuthContext);
+
   const [dataToRender, setDataToRender] = useState(null);
-  const { logout } = useContext(AuthContext);
+  const [isLoading, setIsLoading] = useState(false);
   const [selectedBook, setSelectedBook] = useState(null);
 
   useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const result = await fetchServer({
-          url: 'books',
-          method: 'GET',
-        });
-
-        saveBooksToStorage(result);
-        return result;
-      } catch (error) {
-        console.error('Error fetching data: ', error);
-      }
-    };
-
-    NetInfo.fetch().then(async (state) => {
-      let books = [];
-      if (state.isConnected) {
-        books = await fetchData();
-      } else {
-        books = await getBooksFromStorage();
-      }
-      setDataToRender(books);
-    });
+    fetchData();
   }, []);
 
-  const saveBooksToStorage = async (books) => {
+  const fetchData = async () => {
+    const loadBooksFromStorage = async () => {
+      try {
+        console.log('BOOKS FROM STORAGE');
+        const books = await getBooksFromStorage();
+        return setDataToRender(books);
+      } catch (error) {
+        console.error('Error loading books from storage: ', error);
+      }
+    };
+    //
+
+    setIsLoading(true);
     try {
-      await AsyncStorage.setItem('books', JSON.stringify(books));
+      const { isConnected } = await NetInfo.fetch();
+
+      if (isConnected) {
+        try {
+          const result = await fetchServer({
+            url: '660/books',
+            method: 'GET',
+            token: userToken,
+          });
+          console.log('BOOKS FROM SERVER');
+          saveBooksToStorage(result);
+          setDataToRender(result);
+        } catch (error) {
+          console.error('Error fetching data from the server with internet conection: ', error);
+          loadBooksFromStorage();
+        }
+      } else {
+        loadBooksFromStorage();
+      }
     } catch (error) {
-      console.error('Error saving books: ', error);
+      console.error('Error checking internet connection or fetching data: ', error);
+    } finally {
+      setIsLoading(false);
     }
   };
 
   return (
-    <ScrollView
-      className='flex bg-primary-100 px-4 pt-10 pb-20'
-      contentContainerStyle={{ alignItems: 'center', justifyContent: 'center' }}
+    <TouchableWithoutFeedback
+      onPress={() => {
+        setSelectedBook(null);
+      }}
     >
-      <TouchableOpacity onPress={logout}>
-        <Text>Logout</Text>
-      </TouchableOpacity>
-      {dataToRender?.length > 0 ? (
-        dataToRender.map((book, key) => (
-          <BookCard
-            book={book}
-            key={key}
-            selectedBook={selectedBook}
-            setSelectedBook={setSelectedBook}
-          />
-        ))
-      ) : (
-        <Text>No hay libros</Text>
-      )}
-    </ScrollView>
+      <View className='flex-1 relative'>
+        <ScrollView
+          className='flex bg-primary-100 px-4'
+          contentContainerStyle={{ alignItems: 'center', justifyContent: 'center' }}
+          refreshControl={<RefreshControl refreshing={isLoading} onRefresh={fetchData} />}
+        >
+          {isLoading && (
+            <>
+              <Skeleton className='relative w-full sm:w-64 bg-white shadow-md rounded-md overflow-hidden border border-gray-200 m-2 h-[450px]' />
+              <Skeleton className='relative w-full sm:w-64 bg-white shadow-md rounded-md overflow-hidden border border-gray-200 m-2 h-[450px]' />
+            </>
+          )}
+          {dataToRender?.length > 0 ? (
+            dataToRender.map((book, key) => (
+              <BookCard
+                book={book}
+                key={key}
+                selectedBook={selectedBook}
+                setSelectedBook={setSelectedBook}
+              />
+            ))
+          ) : (
+            <Text className='text-3xl font-bold text-center text-gray-800 mt-4'>
+              No hay libros para mostrar en este momento
+            </Text>
+          )}
+        </ScrollView>
+        <Header className='absolute bottom-0 left-0 right-0 bg-white' />
+      </View>
+    </TouchableWithoutFeedback>
   );
 }
